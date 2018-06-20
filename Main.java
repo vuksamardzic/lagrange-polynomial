2265947c3b3d7394297e0bb514346c70e90dbf9e
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-06-20 10:34:17 +0200

    small tweak on print

--- Main.java
+++ Main.java
@@ -9,11 +9,11 @@ public class Main {
 	public static double prod(double[] n, int xi, double x) {
 		double numerator = 1;
 		double denominator = 1;
-		double func_xi = 0;
+		double func_xi = 1;
 
 		for (int i = 0; i < n.length; i++) {
 			if (i != xi) {
-				System.out.printf("(x[%.4f] - xi[%.4f]) ", x, n[i]);
+				System.out.printf("(x[%.4f] - xj[%.4f]) ", x, n[i]);
 				numerator *= (x - n[i]);
 			} else {
 				func_xi = F(n[xi]);
@@ -21,7 +21,7 @@ public class Main {
 		}
 
 		System.out.printf(
-				"\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- * f(xi)[%f]\n",
+				"\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- * f(xi)[%f]\n",
 				func_xi);
 
 		for (int i = 0; i < n.length; i++) {
@@ -32,14 +32,14 @@ public class Main {
 		}
 		System.out.println("\n\n");
 
-		return (numerator / denominator) * F(x);
+		return (numerator / denominator) * func_xi;
 	}
 
 	public static double lagrange_polynomial(double[] n, double x) {
 		double Ln = 0;
 		for (int i = 0; i < n.length; i++) {
 			double step = prod(n, i, x);
-			System.out.printf("step -> %f \n\n\n", step);
+			System.out.printf("Σ(%d) -> %f \n\n\n", i, step);
 			Ln += step;
 		}
 		return Ln;
@@ -54,8 +54,6 @@ public class Main {
 		double x = 0.3;
 
 		System.out.printf("x = %.4f \nLn(%.4f) = %.4f", x, x, lagrange_polynomial(steps, x));
-		// System.out.println(F(0.4));
-
 	}
 
 }
